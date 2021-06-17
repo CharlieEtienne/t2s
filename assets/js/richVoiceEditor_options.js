b73a197ab8134070f93fa5cce0e14fd5bf239c52
CHARLIE ETIENNE <ce@web-nancy.fr>
2021-06-17 11:01:00 +0200

    SSML - Change for classes and clean up

--- assets/js/richVoiceEditor_options.js
+++ assets/js/richVoiceEditor_options.js
@@ -1,45 +1,35 @@
 const ssmlToolbarOptions = {
     container: [
-        // ['ssml_speak'],
         [{ 'ssml_break': ['weak', 'strong', 'x-strong'] }],
         ['ssml_emphasis'],
-        // ['ssml_whispering'],
-        [{ 'ssml_language': ['en-US', 'en-GB', 'de-DE', 'es-ES', 'fr-FR', 'it-IT'] }],
-        // ['ssml_paragraph'],
-        [{ 'ssml_sayas': ['spell-out', 'number', 'ordinal', 'digits', 'fraction', 'expletive'] }],
-        [{ 'ssml_date': ['mdy', 'dmy', 'ymd', 'md', 'dm', 'ym', 'my', 'd', 'm', 'y'] }],
-        // ['ssml_substitute'],
-        // ['ssml_breaths'],
-        // ['ssml_phoneme'],
-        // ['ssml_parse'],
-        // ['ssml_validate']
+        [{'ssml_rate':['x-slow', 'slow', 'medium', 'fast', 'x-fast']}],
+        [{'ssml_pitch':['x-low', 'low', 'medium', 'high', 'x-high']}],
+        ['ssml_spellout'],
+        // [{ 'ssml_date': ['mdy', 'dmy', 'ymd', 'md', 'dm', 'ym', 'my', 'd', 'm', 'y'] }],
     ],
     handlers: {
-        // 'ssml_speak': function () { },
         'ssml_break': function () { },
         'ssml_emphasis': function () { },
-        'ssml_language': function () { },
-        // 'ssml_paragraph': function () { },
-        'ssml_sayas': function () { },
-        'ssml_date': function () { },
-        // 'ssml_substitute': function () { },
-        // 'ssml_breaths': function () { },
-        // 'ssml_phoneme': function () { },
-        // 'ssml_whispering': function () { },
-        // 'ssml_parse': function () { },
-        // 'ssml_validate': function () { }
+        'ssml_rate': function () { },
+        'ssml_pitch': function () { },
+        'ssml_spellout': function () { },
+        // 'ssml_date': function () { },
     }
 }
-
-Quill.register({
-    'modules/rich-voice-editor': RichVoiceEditor
-})
 
 const quill = new Quill('#text', {
     theme: 'snow',
     modules: {
         toolbar: ssmlToolbarOptions,
-        'rich-voice-editor': true
     },
     placeholder: ''
 });
+
+window.quill = quill;
+
+var BackgroundClass = Quill.import('attributors/class/background');
+var ColorClass      = Quill.import('attributors/class/color');
+var SizeStyle       = Quill.import('attributors/style/size');
+Quill.register(BackgroundClass, true);
+Quill.register(ColorClass, true);
+Quill.register(SizeStyle, true);
