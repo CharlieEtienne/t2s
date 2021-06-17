37ed2a76008e0379743a01aa6c6341607209b012
Pablo <pablo.guyez54@gmail.com>
2021-06-17 15:17:58 +0200

    style to text

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,6 @@ function style_to_code(text) {
     for (let i = 0; i < n; i++) {
         let T=quill.getText(i,1);
         done=false
-        console.log(quill.getFormat(i,1).color);
         if (T=='⌛') {
             code+='<break strength="strong"/>';
         }
@@ -169,109 +168,8 @@ function style_to_code(text) {
                 code+=T
             }
         }}
-    console.log(code);
     return(code);
 }
-
-function style_to_code_old(text) {
-    let code='';
-    let current='normal';
-    let interpret=''
-    let dico=false;
-    let n=text.length;
-    for (let i = 0; i < n; i++) {
-        let T=quill.getText(i,1);
-        if (T=='⌛') {
-            code+='<break strength="strong"/>';
-        }
-        else if (T=='✒') {
-            if (dico==true) {
-                code+='</say-as>';
-                dico=false;
-            }
-            else {
-                code+='<say-as interpret-as="spell-out">';
-                dico=true;
-            }
-        }
-        else {
-
-            console.log(quill.getFormat(i,1).color);
-            if ((quill.getFormat(i,1).color === 'emphasis') && current === 'normal'){
-                code=code + '<emphasis level="strong">'+T;
-                current='emphasis';
-            }
-            else if ((quill.getFormat(i,1).color === 'emphasis') && current !== 'normal'){
-                code+=T;
-            }
-
-
-            if ((quill.getFormat(i,1).color !== 'emphasis') && current === 'emphasis'){
-                code+='</emphasis>'+T;
-                current='normal';
-            }
-            else if ((quill.getFormat(i,1).color !== 'emphasis') && current !== 'emphasis'){
-                code+=T;
-            }
-
-            // TODO : ne marche pas, pb de boucle...
-            let rate_colors = [
-                'rate_x-slow',
-                'rate_slow',
-                'rate_medium',
-                'rate_fast',
-                'rate_x-fast'
-            ];
-
-            rate_colors.forEach(function (item) {
-                let attribute = item.split('rate_')[1];
-                if ((quill.getFormat(i, 1).color === item) && current === 'normal') {
-                    code    = code + '<prosody rate="' + attribute + '">' + T;
-                    current = 'rate';
-                } else if ((quill.getFormat(i, 1).color === item) && current !== 'normal') {
-                    code += T;
-                }
-
-                if ((quill.getFormat(i, 1).color !== item) && current === 'rate') {
-                    code += '</prosody>' + T;
-                    current = 'normal';
-                } else if ((quill.getFormat(i, 1).color !== item) && current !== 'rate') {
-                    code += T;
-                }
-            });
-
-            let pitch_colors = [
-                'pitch_x-low',
-                'pitch_low',
-                'pitch_medium',
-                'pitch_high',
-                'pitch_x-high'
-            ];
-
-            pitch_colors.forEach(function (item) {
-                let attribute = item.split('pitch_')[1];
-                if ((quill.getFormat(i, 1).color === item) && current === 'normal') {
-                    code    = code + '<prosody pitch="' + attribute + '">' + T;
-                    current = 'pitch';
-                } else if ((quill.getFormat(i, 1).color === item) && current !== 'normal') {
-                    code += T;
-                }
-
-                if ((quill.getFormat(i, 1).color !== item) && current === 'pitch') {
-                    code += '</prosody>' + T;
-                    current = 'normal';
-                } else if ((quill.getFormat(i, 1).color !== item) && current !== 'pitch') {
-                    code += T;
-                }
-            });
-
-
-  
-        }
-    }
-
-      return(code);
-  }
 
 
 const audio         = new Audio();
